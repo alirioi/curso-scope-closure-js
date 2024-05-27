074a8af599180d427a0b7613aac344a67483cf5a
isealirio <isealirio@gmail.com>
2024-05-27 18:03:43 +0000

    Agregando la cuarta clase

---
+++ src/scope/re.js
@@ -0,0 +1,34 @@
+var firstName; // undefined
+firstName = 'Oscar';
+console.log(firstName);
+
+var lastName = 'David'; // declarar y asignar
+lastName = 'Ana'; // reasignar
+console.log(lastName)
+
+var secondName = 'David'; // declarando y asignando
+var secondName = 'Ana'; // reasignando
+console.log(secondName);
+
+// Let
+let fruit = 'Apple' // declarar y asignar
+fruit = 'Kiwi' // reasignar
+console.log(fruit)
+
+let fruit = 'Banana'
+console.log(fruit)
+
+// const
+const animal = 'dog' // declarar y asignar
+animal = 'cat' // reasignar
+console.log(animal)
+
+const animal = 'snake'
+console.log(animal)
+
+const vehicles = [];
+vehicles.push("car")
+console.log(vehicles)
+
+vehicles.pop();
+console.log(vehicles)
